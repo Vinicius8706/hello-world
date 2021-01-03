142196f096e66f317123e8e829304c93657cfbee
Vinicius8706 <76622945+Vinicius8706@users.noreply.github.com>
2021-01-02 22:52:42 -0300

    Operadores
    Calcular o IMC de uma pesosa

---
+++ operadores.js
@@ -0,0 +1,11 @@
+const idade = 18
+
+if(!(idade >= 18) || idade === 17){
+    console.log('Bloquear entrada')
+}else{
+    console.log('Deixar entrar')
+}
+
+console.log(2*2)
+console.log(2/2)
+console.log(2 % 1.5)
